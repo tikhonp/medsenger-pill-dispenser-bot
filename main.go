a289016f3ac6a62f36a67b6307eea15d44ef830d
TikhonP <tikhon.petrishchev@gmail.com>
2025-03-18 10:52:40 +0300

    Added submit handler

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/binary"
 	"fmt"
+	"io"
 	"net/http"
 	"strconv"
 	"time"
@@ -14,32 +15,56 @@ import (
 const ContentTypeOctetStream = "application/octet-stream"
 
 func pillsSchedulerHandler(c echo.Context) error {
-    serialNumber := c.QueryParam("serial_number")
-    if serialNumber != "" {
-        fmt.Println("Scheduler request from:", serialNumber)
+	serialNumber := c.QueryParam("serial_number")
+	if serialNumber != "" {
+		fmt.Println("Scheduler request from:", serialNumber)
+	}
+
+	var cellsCount = 4
+	cellsCountStr := c.QueryParam("cells_count")
+	if cellsCountP, err := strconv.Atoi(cellsCountStr); err == nil {
+		cellsCount = cellsCountP
+	}
+
+	out := make([]byte, 0, cellsCount*4)
+	now := time.Now()
+	for i := range cellsCount {
+		// make array of timestamps from now and every minute after
+		timestamp := uint32(now.Add(time.Minute * time.Duration(i)).Unix())
+		out = binary.BigEndian.AppendUint32(out, timestamp)
+	}
+
+	return c.Blob(http.StatusOK, ContentTypeOctetStream, out)
+}
+
+func submitPill(c echo.Context) error {
+
+	body, err := io.ReadAll(c.Request().Body)
+	if err != nil {
+		return err
+	}
+    if len(body) < 5 {
+        return echo.NewHTTPError(http.StatusBadRequest, "data length must be at least 5 bytes")
     }
 
-    var cellsCount = 4
-    cellsCountStr := c.QueryParam("cells_count")
-    if cellsCountP, err := strconv.Atoi(cellsCountStr); err == nil {
-        cellsCount = cellsCountP
-    }
+	var timestamp uint32
+	_, err = binary.Decode(body[:4], binary.BigEndian, &timestamp)
+	if err != nil {
+		return err
+	}
+	submitTime := time.Unix(int64(timestamp), 0)
+	cellIndx := body[4]
+	serialNumber := string(body[5:])
 
-    out := make([]byte, 0, cellsCount*4)
-    now := time.Now()
-    for i := range cellsCount {
-        // make array of timestamps from now and every minute after
-        timestamp := uint32(now.Add(time.Minute * time.Duration(i)).Unix())
-        out = binary.BigEndian.AppendUint32(out, timestamp)
-    }
+	fmt.Printf("Submit pill request: [time > %v cell > %d serialn > %s]\n", submitTime, cellIndx, serialNumber)
 
-    return c.Blob(http.StatusOK, ContentTypeOctetStream, out)
+	return c.NoContent(http.StatusOK)
 }
 
 func main() {
 	e := echo.New()
 	e.HideBanner = true
-    e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
 		Format: "[${time_rfc3339}] ${status} ${method} ${path} (${remote_ip}) ${latency_human}\n",
 		Output: e.Logger.Output(),
 	}))
@@ -48,9 +73,10 @@ func main() {
 		return c.String(http.StatusOK, "Купил мужик шляпу, а она ему как раз!")
 	})
 	e.GET("/schedule", pillsSchedulerHandler)
-    e.POST("/status", func(c echo.Context) error {
-        return c.JSON(http.StatusOK, map[string]interface{}{"is_tracking_data": true, "supported_scenarios": []int{}, "tracked_contracts": []int{}})
-    })
+	e.POST("/submit", submitPill)
+	e.POST("/status", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, map[string]interface{}{"is_tracking_data": true, "supported_scenarios": []int{}, "tracked_contracts": []int{}})
+	})
 
 	e.Logger.Fatal(e.Start(":3054"))
 }
