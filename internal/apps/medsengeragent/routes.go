06bddf15b814ea618c969ec32a9deec40fc2cf65
TikhonP <tikhon.petrishchev@gmail.com>
2025-09-16 21:23:02 +0300

    Added logging for agents

--- internal/apps/medsengeragent/routes.go
+++ internal/apps/medsengeragent/routes.go
@@ -3,6 +3,7 @@ package medsengeragent
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/tikhonp/medsenger-pill-dispenser-bot/internal/apps/medsengeragent/handlers"
 	"github.com/tikhonp/medsenger-pill-dispenser-bot/internal/util"
 )
@@ -11,6 +12,11 @@ func ConfigureMedsengerAgentGroup(g *echo.Group, deps util.Dependencies) {
 	mah := handlers.MedsengerAgentHandler(deps)
 
 	g.Use(util.APIKeyJSON(deps.Cfg.Server))
+
+	g.Use(middleware.BodyDump(func(c echo.Context, reqBody, resBody []byte) {
+		c.Logger().Infof("Request: %s %s, Body: %s", c.Request().Method, c.Request().URL, string(reqBody))
+		c.Logger().Infof("Response: %s %s, Body: %s", c.Request().Method, c.Request().URL, string(resBody))
+	}))
 
 	g.POST("/init", mah.Init)
 	g.POST("/status", mah.Status)
