9fde6d32db5a5bf8105c1fa6402c1f18fd4f8c8d
TikhonP <tikhon.petrishchev@gmail.com>
2025-09-17 16:34:16 +0300

    Removed medsenger API logging

--- internal/apps/medsengeragent/routes.go
+++ internal/apps/medsengeragent/routes.go
@@ -2,10 +2,7 @@
 package medsengeragent
 
 import (
-	"fmt"
-
 	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
 	"github.com/tikhonp/medsenger-pill-dispenser-bot/internal/apps/medsengeragent/handlers"
 	"github.com/tikhonp/medsenger-pill-dispenser-bot/internal/util"
 )
@@ -15,9 +12,9 @@ func ConfigureMedsengerAgentGroup(g *echo.Group, deps util.Dependencies) {
 
 	g.Use(util.APIKeyJSON(deps.Cfg.Server))
 
-	g.Use(middleware.BodyDump(func(c echo.Context, reqBody, resBody []byte) {
-		fmt.Printf("Request Body: \n%s\n", reqBody)
-	}))
+	// g.Use(middleware.BodyDump(func(c echo.Context, reqBody, resBody []byte) {
+	// 	fmt.Printf("Request Body: \n%s\n", reqBody)
+	// }))
 
 	g.POST("/init", mah.Init)
 	g.POST("/status", mah.Status)
